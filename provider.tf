7651cdc2ebdf424acb45de085e70a97fdae2a2e6
umairdevops <mohammedumair836@gmail.com>
2023-09-11 21:37:14 +0530

    ec2

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+    access_key = var.AWS_ACCESSKEY
+    secret_key = var.AWS_SECRETKEY
+    region = var.AWS_REGION
+}
+
+
---
+++ variable.tf
@@ -0,0 +1,26 @@
+# # Access Key
+variable AWS_ACCESSKEY {
+  default     = "AKIATIKJPAXQYTRX234M"
+}
+
+# Secret Key
+variable AWS_SECRETKEY {
+  default     = "t+4zMnJuAlqpnFN3IZMpBC5SWEVj3HblT+/WppxB"
+}
+
+# Region
+variable AWS_REGION {
+  default     = "ap-south-1"
+}
+
+#AMI
+variable AMI {
+  default = "ami-08e5424edfe926b43"
+}
+
+#instance_type
+variable machine_type {
+  default = "t2.micro"
+}
+
+
